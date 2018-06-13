81d9c93de77cf6ceb8aff0b31ecd5fe354c814de
alexthedar <alex.castaneda@gmail.com>
2018-06-13 11:57:47 -0700

    split string to array and findindex of removes but retains upper and lower diff

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -13,7 +13,7 @@ rl.on('line', (string)=> {
 function dedup(string){
   let isValid = validate(string);
   if(isValid === true){
-    console.log(`> Deduped string: ${string}`)
+    console.log(`> Deduped string: ${removeDuplicates(string)}`)
     rl.close()
   } else {
     console.log('> Only UPPER or lowercase letters may be entered.')
@@ -23,4 +23,17 @@ function dedup(string){
 function validate(str){
   let RegExpression = /^[a-zA-Z\s]*$/; 
   return RegExpression.test(str)
+}
+
+function removeDuplicates(str){
+  let stringArray = str.split('')
+  let finalArr = []
+
+  for(let i = 0; i< stringArray.length; i++){
+    if (!stringArray.includes(stringArray[i].toLowerCase())) {
+      finalArr.push(stringArray[i])
+    }
+  }
+
+  return finalArr.join('');
 }
